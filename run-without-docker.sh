feb988db9e690ccbd6882d0406d71e11ddcb7c38
Intronirisme <38733938+Intronirisme@users.noreply.github.com>
2025-07-31 10:48:16 +0200

    Added local des env

--- run-without-docker.sh
+++ run-without-docker.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 source venv/bin/activate
-
-export SECRET_KEY="xk&a#nle_=9!(+z_hu*g+llh)3j^hp+ymk)i_v5zf87+mm@*+h"
-
-export DOMAIN="localhost"
-export TARGET="development"
-
-export ALLOWED_HOSTS=".localhost, .0.0.0.0"
-
-export DJANGO_SETTINGS_MODULE="backend.settings.local"
+source local-development.env
 
 cd app/
 python manage.py runserver
